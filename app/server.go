95a99c3146b4343de42cef1e686c0ed6da1acbdd
SomberTM <sombertm@gmail.com>
2024-07-01 18:53:37 -0400

    Support concurrent connection with goroutines

--- app/server.go
+++ app/server.go
@@ -93,25 +93,8 @@ func (res *HttpResponse) writable() []byte {
 	return buf.Bytes()
 }
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	fmt.Println("Listening on port 4221")
-	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	defer conn.Close()
+func handleConnection(conn net.Conn) {
+defer conn.Close()
 
 	buf := make([]byte, 4096)
 	n, err := conn.Read(buf)
@@ -135,4 +118,27 @@ func main() {
 
 	fmt.Println("Response", response)
 	conn.Write(response.writable())
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	fmt.Println("Listening on port 4221")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
 }
